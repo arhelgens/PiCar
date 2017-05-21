fe26735b0bb6dff51188b6944fb8108d5dea7208
anthony r helgens <t_helgens@hotmail.com>
2017-05-21 08:43:21 -0500

    Changes

--- PiCar/MainPage.xaml.cs
+++ PiCar/MainPage.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -13,18 +17,86 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace PiCar
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
+            InitializeDB();
+
             this.InitializeComponent();
+            var test = Task.Run(() => GetSubscribedListAsync()).Result;
+            var listToDispaly = new List<Episode>();
+            foreach (var item in test)
+            {
+                var tempList = Task.Run(() => ListEpisodesAsync(item.name, item.url, item.key)).Result;
+                listToDispaly.AddRange(tempList);
+            }
+
+            lv_Episodes.ItemsSource = listToDispaly;
+        }
+        private async Task<List<Podcast>> GetSubscribedListAsync()
+        {
+            var subList = new List<Podcast>();
+            var localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+            var existingFile = await localFolder.TryGetItemAsync("PodcastDB.db");
+            var conString = "Data Source=" + existingFile.Path + ";";
+            var cmdString = "SELECT * FROM PodcastMaster";
+            SqliteConnection cn = new SqliteConnection(conString);
+            SqliteCommand cmd = new SqliteCommand(cmdString, cn);
+            cn.Open();
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                var curPodcast = new Podcast();
+                curPodcast.key = Convert.ToInt32(reader["PodcastKey"]);
+                curPodcast.name = reader["PodcastName"].ToString();
+                curPodcast.url = reader["PodcastURL"].ToString();
+                subList.Add(curPodcast);
+            }
+            cn.Close();
+            return subList;
+        }
+        private async Task<List<Episode>> ListEpisodesAsync(string name, string url, int key)
+        {
+            Podcast pc = new Podcast();
+            pc.key = key;
+            pc.name = name;
+            pc.url = url;
+            await pc.GetEpisodeListAsync();
+            return pc.episodesList;
+        }
+
+        private async void InitializeDB()
+        {
+            //var dbFile = ApplicationData.Current.LocalFolder.TryGetItemAsync("\\Assets\\PodcastDB.db") as StorageFile;
+
+            ////await ApplicationData.Current.LocalFolder.TryGetItemAsync("PodcastDB.db") as StorageFile;
+
+            //if (null == dbFile)
+            //{
+            //    var localFolder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+
+            //    string dbFileName = @"Assets\PodcastDB.db";
+            //    var originalDbFile = await localFolder.GetFileAsync(dbFileName);
+            //    if (null != originalDbFile)
+            //        dbFile = await originalDbFile.CopyAsync(localFolder, "PodcastDB.db", NameCollisionOption.ReplaceExisting);
+            //}
+            // Check if the file is already present in the Local folder
+            var localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+            var existingFile = await localFolder.TryGetItemAsync("PodcastDB.db");
+
+            if (existingFile == null)
+            {
+                // Copy the file from the install folder to the local folder
+                var folder = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync("Assets");
+                var file = await folder.GetFileAsync("PodcastDB.db");
+                if (file != null)
+                {
+                    await file.CopyAsync(localFolder, "PodcastDB.db", Windows.Storage.NameCollisionOption.FailIfExists);
+                }
+            }
         }
     }
 }
---
+++ PiCar/Podcast.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Xml;
+using System.Xml.Linq;
+using System.Net.Http;
+using System.Diagnostics;
+using Microsoft.Data.Sqlite;
+
+namespace PiCar
+{
+    class Episode
+    {
+        public string podcastName;
+        public string name;
+        public string url;
+        public string status = null;
+        public DateTime pubDate;
+    }
+    class Podcast
+    {
+        public int key;
+        public string url;
+        public string name;
+        public List<Episode> episodesList;
+
+        public async Task GetEpisodeListAsync()
+        {
+            episodesList = new List<Episode>();
+            var httpClient = new HttpClient();
+            var httpResponse = await httpClient.GetAsync(url);
+            httpResponse.EnsureSuccessStatusCode();
+            var httpResponseBody = await httpResponse.Content.ReadAsStringAsync();
+            TextReader reader = new StringReader(httpResponseBody);
+
+            XDocument rssFeed = XDocument.Load(reader);
+            foreach (var item in rssFeed.Descendants("item"))
+            {
+                var curEpisode = new Episode();
+                curEpisode.name = item.Element("title").Value;
+                if (!DateTime.TryParse(item.Element("pubDate").Value, out curEpisode.pubDate))
+                    curEpisode.pubDate = Convert.ToDateTime("1984-01-19");
+                curEpisode.url = item.Element("enclosure").Attribute("url").Value;
+                curEpisode.podcastName = name;
+                episodesList.Add(curEpisode);
+            }
+            await GetEpisodeHistory();
+        }
+        private async Task GetEpisodeHistory()
+        {
+            var localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+            var existingFile = await localFolder.TryGetItemAsync("PodcastDB.db");
+            var conString = "Data Source=" + existingFile.Path + ";";
+            var cmdString = "SELECT * FROM Episodes";
+            SqliteConnection cn = new SqliteConnection(conString);
+            SqliteCommand cmd = new SqliteCommand(cmdString, cn);
+            var episodeStatus = new List<Episode>();
+            cn.Open();
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                var curEpisode = new Episode();
+                curEpisode.podcastName = name;
+                curEpisode.url = reader["URL"].ToString();
+                curEpisode.status = reader["Status"].ToString();
+                curEpisode.name = reader["Title"].ToString();
+                episodeStatus.Add(curEpisode);
+            }
+            cn.Close();
+            foreach (var item in episodesList)
+            {
+                item.status = episodeStatus.Where(x => x.url == item.url)
+                    .Select(x => x.status)
+                    .FirstOrDefault();
+            }
+            var needsUpdateList = episodesList.Where(x => x.status == null).ToList();
+            episodesList = episodesList.Where(x => x.status != "Deleted").ToList();
+            Task.Run(()=> UpdateDB(needsUpdateList));
+        }
+
+        private async void UpdateDB(List<Episode> updatedList)
+        {
+            var localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+            var existingFile = await localFolder.TryGetItemAsync("PodcastDB.db");
+
+            var conString = "Data Source=" + existingFile.Path + ";";
+            string cmdString = "";
+            SqliteConnection cn = new SqliteConnection(conString);
+            SqliteCommand cmd = new SqliteCommand(cmdString, cn);
+            cn.Open();
+            foreach (var item in updatedList)
+            {
+                cmd.CommandText = String.Format("INSERT INTO Episodes (PodcastKey, Title, URL, Status) " +
+                    "SELECT PodcastKey, '{0}', '{1}', 'New' "+
+                    "FROM PodcastMaster " +
+                    "WHERE PodcastName = '{2}'", item.name.Replace("'",""), item.url, name);
+                cmd.ExecuteNonQuery();
+            }
+            cn.Close();
+        }
+    }
+}
